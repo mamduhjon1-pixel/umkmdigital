ee05460237c2577e83864365213b252c73982ec6
mamduhjon1-pixel <mamduhjon1@gmail.com>
2026-05-18 20:37:34 +0900

    Add files via upload

---
+++ dist/assets/index-DjiQK30v.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-Bk7jFdw-.js","./index-Dxkv_Nq-.js","./index-KUFwy_jk.css"])))=>i.map(i=>d[i]);
+import{K as p,_ as r}from"./index-Dxkv_Nq-.js";const o=p("App",{web:()=>r(()=>import("./web-Bk7jFdw-.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
