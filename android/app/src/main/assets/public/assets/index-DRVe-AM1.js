46b0c7577bce77cc47182fcedb2c3585e1b6836f
mamduhjon1-pixel <mamduhjon1@gmail.com>
2026-05-18 20:40:38 +0900

    Add files via upload

---
+++ android/app/src/main/assets/public/assets/index-DRVe-AM1.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-D_srSZg_.js","./index-Dxkv_Nq-.js","./index-KUFwy_jk.css"])))=>i.map(i=>d[i]);
+import{K as r,_ as t}from"./index-Dxkv_Nq-.js";const _=r("SplashScreen",{web:()=>t(()=>import("./web-D_srSZg_.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.SplashScreenWeb)});export{_ as SplashScreen};
